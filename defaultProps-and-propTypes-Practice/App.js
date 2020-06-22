1a69d84c24b423758df8e44ac94e321f63a97a03
unknown <kevj.m13@gmail.com>
2020-06-22 16:36:42 -0400

    notes

---
+++ defaultProps-and-propTypes-Practice/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import RoundedImg from "./RoundedImg"
+
+function App() {    
+    return (
+        <div>
+            <RoundedImg src="https://picsum.photos/id/237/300/300" />
+        </div>
+    )
+}
+
+export default App
